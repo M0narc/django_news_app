1163e51e87115ce8ee36c175c1c2c401793cdb17
VictorVagabculov <vvagabculov@gmail.com>
2024-10-06 01:06:00 -0300

    admin de articulo y migraciones

--- src/apps/Articulo/admin.py
+++ src/apps/Articulo/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Articulo
 
 class ArticuloCustom(admin.ModelAdmin):
-    fields = ('fecha_publicada','titulo',)
-    list_display = ('fecha_publicada',)
-    search_fields = ('titulo',)
-
+    fields = ('fecha_publicada','titulo','autor')
+    list_display = ('fecha_publicada','autor', 'titulo', )
+    search_fields = ('titulo','autor__nombre', 'autor__apellido')
+    list_per_page = 20
 admin.site.register(Articulo, ArticuloCustom)
