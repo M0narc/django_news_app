ed877cd6cec6b80867560d572c130be68e1a1470
VictorVagabculov <vvagabculov@gmail.com>
2024-10-08 23:00:07 -0300

    navbar

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_BASE_DIR = BASE_DIR / "static"
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors" #PREGUNTAR A FRANCO
 
 # Media -> Imagenes, audios, videos que van surgiendo o creciendo a lo largo de la ejecución.
