a6d1267a2b71826fdbc2ff4a41a1efa74a073e48
M0narc <francomatiaslujan23@gmail.com>
2024-10-10 13:44:43 -0300

    adding categories to articulo

--- src/apps/articulo/admin.py
+++ src/apps/articulo/admin.py
@@ -1,29 +1,39 @@
 from django.contrib import admin
-from .models import Articulo
+from .models import Articulo, Categoria
 
-# esto es un decorator
 @admin.register(Articulo)
 class ArticuloCustomAdmin(admin.ModelAdmin):
 
     list_display = ('titulo', 
                     'autor', 
+                    'categoria',
                     'fecha_publicada')
 
-    # busqueda por titulo y nombre del autor
+    # buscar por titulo, nombre del autor y categoría
     search_fields = ('titulo', 
                      'autor__nombre', 
-                     'autor__apellido')
+                     'autor__apellido',
+                     'categoria__nombre')
 
-    # filtros laterales por autor y fecha publicada
+    # Filtros laterales por autor, fecha publicada y categoría
     list_filter = ('autor', 
-                   'fecha_publicada')
+                   'fecha_publicada',
+                   'categoria')
 
-    # campos para el formulario de creación/edición
+    # formulario de creación/edición
     fields = ('titulo', 
               'contenido', 
               'autor', 
+              'categoria',  # Agregar la categoría en el formulario
               'fecha_publicada')
 
-    # opcion de ordenar los articulos por fecha publicada y título
+    # opcion de ordenar los artículos por fecha publicada y título
     ordering = ('fecha_publicada', 
                 'titulo')
+
+@admin.register(Categoria)
+class CategoriaCustomAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+    search_fields = ('nombre',)
+    # para poder crear las nuevas categorias
+    fields = ('nombre',)
--- src/apps/articulo/models.py
+++ src/apps/articulo/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.utils import timezone
 from apps.autor.models import Autor
 
+# para poder hacer dinamica la categoria
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
 class Articulo(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100)
     contenido = models.TextField(null=False)
     autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, 
+                                  null=True, 
+                                  blank=True)
     fecha_publicada = models.DateField(default=timezone.now)
    
     def __str__(self):
