6176a6c19c7a477b8b72176f3bb45ba5420010a9
M0narc <francomatiaslujan23@gmail.com>
2024-10-12 14:40:44 -0300

    categorias agregadas al navbar

--- src/apps/articulo/views.py
+++ src/apps/articulo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .models import Articulo
+from .models import Categoria, Articulo
 
 def home_view(request):
+    categorias = Categoria.objects.all()
     articulos = Articulo.objects.all().order_by('-fecha_publicada')[:5]  # Últimos 5 artículos
-    return render(request, 'home.html', {'articulos': articulos})
+
+    context = {
+        'articulos': articulos,
+        'categories': categorias,
+    }
+    
+    return render(request, 'home.html', context)
